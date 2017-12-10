ae0843d5423a6cfe22af03c665aa6111b112308e
Oleksandr Kovaliuk <kovaliukoleksandr@gmail.com>
2017-12-11 00:52:27 +0200

    Add cart

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,79 @@
-var bag-price = 199,
-    belt-price = 49,
-    shoes-price = 199,
-    bag-quantity = 1,
-    belt-quantity = 1, 
-    shoes-quantity = 1,
-    bag-total = bag-price * bag-quantity,
-    belt-total = belt-price * belt-quantity,
-    shoes-total = shoes-price * shoes-quantity,
-    subtotal = bag-total + belt-total + shoes-total;
+var bagprice = 199,
+beltprice = 49,
+shoesprice = 199,
+bagquantity = 1,
+beltquantity = 1, 
+shoesquantity = 1,
+bagtotal = 199,
+belttotal = 49,
+shoestotal = 199,
+subtotal;
 
-function plusBag() {
-    bag-quantity++;
-    document.querySelector('#bag-quantity').innerHTML = bag-quantity;
-    documet.querySelector('#bag-total').innerHTML = '&#36;' + bag-total;
+document.querySelector('#bag-plus').onclick = function() {
+bagquantity++;
+bagtotal = bagprice * bagquantity;
+subtotal = bagtotal + belttotal + shoestotal;
+document.querySelector('#bag-quantity').innerHTML = bagquantity;
+document.querySelector('#bag-total').innerHTML = '&#36;' + bagtotal;
+document.querySelector('#subtotal').innerHTML = '&#36;' + subtotal;
 }
 
+document.querySelector('#bag-minus').onclick = function() {
+bagquantity--;
+bagtotal = bagprice * bagquantity;
+subtotal = bagtotal + belttotal + shoestotal;
+document.querySelector('#subtotal').innerHTML = '&#36;' + subtotal;
+if (bagquantity == 0) {
+ document.querySelector('.cart__line--bag').style.display = 'none';
+}
+else {              
+document.querySelector('#bag-quantity').innerHTML = bagquantity;
+document.querySelector('#bag-total').innerHTML = '&#36;' + bagtotal;              
+}
+}
+
+document.querySelector('#belt-plus').onclick = function() {
+beltquantity++;
+belttotal = beltprice * beltquantity;
+subtotal = bagtotal + belttotal + shoestotal;
+document.querySelector('#belt-quantity').innerHTML = beltquantity;
+document.querySelector('#belt-total').innerHTML = '&#36;' + belttotal;
+document.querySelector('#subtotal').innerHTML = '&#36;' + subtotal;
+}
+
+document.querySelector('#belt-minus').onclick = function() {
+beltquantity--;
+belttotal = beltprice * beltquantity;
+subtotal = bagtotal + belttotal + shoestotal;
+document.querySelector('#subtotal').innerHTML = '&#36;' + subtotal;
+if (beltquantity == 0) {
+ document.querySelector('.cart__line--belt').style.display = 'none';
+}
+else {              
+document.querySelector('#belt-quantity').innerHTML = beltquantity;
+document.querySelector('#belt-total').innerHTML = '&#36;' + belttotal;              
+}
+}
+
+document.querySelector('#shoes-plus').onclick = function() {
+shoesquantity++;
+shoestotal = shoesprice * shoesquantity;
+subtotal = bagtotal + belttotal + shoestotal;
+document.querySelector('#shoes-quantity').innerHTML = shoesquantity;
+document.querySelector('#shoes-total').innerHTML = '&#36;' + shoestotal;
+document.querySelector('#subtotal').innerHTML = '&#36;' + subtotal;
+}
+
+document.querySelector('#shoes-minus').onclick = function() {
+shoesquantity--;
+shoestotal = shoesprice * shoesquantity;
+subtotal = bagtotal + belttotal + shoestotal;
+document.querySelector('#subtotal').innerHTML = '&#36;' + subtotal;
+if (shoesquantity == 0) {
+ document.querySelector('.cart__line--shoes').style.display = 'none';
+}
+else {        
+document.querySelector('#shoes-quantity').innerHTML = shoesquantity;
+document.querySelector('#shoes-total').innerHTML = '&#36;' + shoestotal;
+}
+}
